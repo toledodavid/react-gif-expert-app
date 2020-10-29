9afaa640007dbef549f7366ee9b32a414275dac1
David Toledo <dtoledoz0207@gmail.com>
2020-10-29 10:48:20 -0600

    create-list-catogories-in-GifExpertApp-component

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GifExpertApp = () => {
+
+  //const categories = ['One punch', 'Samurai X', 'Dragon Ball'];
+  const [categories, setCategories] = useState(['One punch', 'Samurai X', 'Dragon Ball']);
+
+
+  const handleAdd = () => {
+    // setCategories([...categories, 'Daredevil']);
+    setCategories(category => [...categories, 'Batman']);
+  }
+
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr/>
+
+      <button onClick={handleAdd}>Add</button>
+
+      <ol>
+        {
+          categories.map(category => {
+          return <li key={category}>{category}</li>
+          })
+        }
+      </ol>
     </>
   );
 }
