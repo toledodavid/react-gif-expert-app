ccbf665a664d419706917bb0f5071f2d00dd9462
David Toledo <dtoledoz0207@gmail.com>
2020-11-02 11:34:46 -0600

    add-animations-from-animate.css-library

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -4,7 +4,7 @@ import React from 'react';
 const GifGridItem = ({title, imgUrl}) => {
 
   return(
-    <div className="card">
+    <div className="card animate__animated animate__fadeIn">
       <img src={imgUrl} alt={title} />
       <p>{title}</p>
     </div>
