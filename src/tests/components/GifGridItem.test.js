53598d8d5b8155490263ecc128ed243fbafab0d7
David Toledo <dtoledoz0207@gmail.com>
2020-11-03 12:17:18 -0600

    create-tests-for-GifGridItem-component

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -8,8 +8,26 @@ describe('Tests for <GifGridItem/> component', () => {
   const title = 'Test title';
   const imgUrl = 'https://www.google.com';
 
+  const wrapper = shallow(<GifGridItem title={title} imgUrl={imgUrl} />);
+
   test('It should render <GifGridItem/> correctly', () => {
-    const wrapper = shallow(<GifGridItem title={title} imgUrl={imgUrl} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('It should have a paragraph with the title', () => {
+    const p = wrapper.find('p');
+    expect(p.text().trim()).toBe(title);
+  });
+
+  test('It should have a src with value of imgUrl and an alt with value of title', () => {
+    const img = wrapper.find('img');
+    expect(img.prop('src')).toBe(imgUrl);
+    expect(img.prop('alt')).toBe(title);
+  });
+
+  test('It should have css class animate__fadeIn', () => {
+    const div = wrapper.find('div');
+    const className = div.prop('className');
+    expect(className.includes('animate__fadeIn')).toBe(true); 
   });
 });
