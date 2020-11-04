a8b03213daa8bd439c87f15d0be9d69c2d6f911b
David Toledo <dtoledoz0207@gmail.com>
2020-11-04 10:56:04 -0600

    create-test-for-onChange-in-input-from-AddCategory-component

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -21,6 +21,7 @@ const AddCategory = ({setCategories}) => {
 
   return (
     <form onSubmit={handleSubmit}>
+      <p>{inputValue}</p>
       <input
         type="text"
         placeholder={inputValue}
--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -6,10 +6,18 @@ import AddCategory from '../../components/AddCategory';
 describe('Tests for <AddCategory /> component', () => {
 
   const setCategories = () => {};
+  const wrapper = shallow(<AddCategory setCategories={setCategories} />);
 
   test('It should render correctly', () => {
-    const wrapper = shallow(<AddCategory setCategories={setCategories} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('It should change input value', () => {
+    const input = wrapper.find('input');
+    const value = 'Mario Bros';
+    input.simulate('change', {target: {value}});
+
+    expect(wrapper.find('p').text().trim()).toBe(value);
   });
 
 });
