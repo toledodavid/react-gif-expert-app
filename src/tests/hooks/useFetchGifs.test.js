8bff0d98f57b5b6b914f4dbccbc83ed655562648
David Toledo <dtoledoz0207@gmail.com>
2020-11-06 11:17:32 -0600

    create-second-test-for-useFetchGifs-hook-with-waitForNextUpdate-method-from-renderHook

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -4,14 +4,27 @@ import { useFetchGifs } from '../../hooks/useFetchGifs';
 
 describe('Tests in useFetchGifs hook', () => {
 
-  test('It should return initial state', () => {
+  test('It should return initial state', async() => {
 
-    const { result } = renderHook(() => useFetchGifs('Batman'));
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('Batman'));
 
     const { data, loading } = result.current;
 
+    await waitForNextUpdate();
+
     expect(data).toEqual([]);
     expect(loading).toBe(true);
+  });
+
+  test('It should return images array and loading in false', async() => {
+
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('Batman'));
+    await waitForNextUpdate();
+
+    const { data, loading } = result.current;
+
+    expect(data.length).toBe(10);
+    expect(loading).toBe(false);
   });
 
 });
